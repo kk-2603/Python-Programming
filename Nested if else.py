c7358ed70c2c03353b4868c7a22f886fad8c39cf
kkv <88773885+kk-2603@users.noreply.github.com>
2021-11-21 22:55:30 +0530

    Add files via upload

---
+++ Nested if else.py
@@ -0,0 +1,10 @@
+x = input("Please enter a number= ")
+y = input("Please enter a number= 7")
+if x >= y:
+    print("x is greater than or equals to y")
+    if x == y:
+        print("x is equals to y")
+    else:
+        print("x is greater than y")
+else:
+    print("x is less than y")
