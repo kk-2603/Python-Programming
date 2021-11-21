07cb017fa944decc787abc5480355a90df879b1b
kkv <88773885+kk-2603@users.noreply.github.com>
2021-11-21 22:27:55 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,13 @@
+# Python Program to find Area Of Circle accepting radius as input
+
+
+r = float(input(' Please Enter the radius of a circle: '))
+
+# compute area
+
+area = 3.142 * r *r
+
+# print area to output screen
+print("Radius= ",r)
+print(" Area Of a Circle = %.2f" %area)
+
